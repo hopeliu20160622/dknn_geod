6b8047de19d8dec40a1d0f20e0899041bdcbb985
Cristian Challu <cchallu@GS19963.wv.cc.cmu.edu>
2019-10-25 17:40:05 -0400

    original dknn, toy example, setup

--- README.md
+++ README.md
@@ -5,4 +5,16 @@ Repository intended to explore extensions to DkNN using geodesic distances.
 <p float="center">
   <img src="data/deep_knn.png" width="200" />
   <img src="data/geodesic.png" width="240" /> 
+  <img src="data/dknn_test.png" width="240" /> 
 </p>
+
+# Conda environment
+```console
+local_user@local_host$ bash setup.sh
+```
+
+# Run original DKNN (from cleverhans)
+```console
+local_user@local_host$ conda activate dknn_geod
+local_user@local_host$ python src/dknn.py
+```
---
+++ setup.sh
@@ -0,0 +1,20 @@
+# reachability packages
+conda create --name dknn_geod python=3.6
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate dknn_geod
+conda install -c conda-forge matplotlib
+conda install -c anaconda scikit-learn
+
+pip install --upgrade pip
+pip install tensorflow==1.15.0
+#pip install tensor2tensor==1.14.1
+#t2t-datagen --generate_data --data_dir=~/t2t_data --problem=image_cifar10_plain
+#t2t-datagen --generate_data --data_dir=~/t2t_data --problem=image_cifar100_plain
+
+conda install -c conda-forge jupyterlab
+ipython kernel install --user --name=dknn_geod
+conda install -c anaconda pandas
+pip install cleverhans
+conda install -c akode falconn
+
+conda deactivate
