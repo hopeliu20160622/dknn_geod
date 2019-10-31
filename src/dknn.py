4fd1df03501b407f7c57a34f3dfc3c9b82a5d49f
Kin <kin_gutierrez@Kins-MBP-2.wv.cc.cmu.edu>
2019-10-30 20:12:05 -0400

    Compatibility of DkNNGeod and DKNNOriginal

--- src/dknn.py
+++ src/dknn.py
@@ -32,18 +32,17 @@ from cleverhans import serial
 from pathlib import Path
 from utils_kernel import euclidean_kernel, hard_geodesics_euclidean_kernel
 
+###################################
+# TENSORFLOW UTILS
+###################################
+
 FLAGS = tf.flags.FLAGS
 
 def get_tensorflow_session():
-    gpu_options = tf.GPUOptions()
-    gpu_options.per_process_gpu_memory_fraction=FLAGS.tensorflow_gpu_memory_fraction
-    sess = tf.Session(
-        config=tf.ConfigProto(
-            gpu_options=gpu_options
-        )
-    )
-
-    return sess
+  gpu_options = tf.GPUOptions()
+  gpu_options.per_process_gpu_memory_fraction=FLAGS.tensorflow_gpu_memory_fraction
+  sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+  return sess
 
 def make_basic_picklable_cnn(nb_filters=64, nb_classes=10,
                              input_shape=(None, 28, 28, 1)):
@@ -60,6 +59,145 @@ def make_basic_picklable_cnn(nb_filters=64, nb_classes=10,
             Softmax()]
   model = MLP(layers, input_shape)
   return model
+
+###################################
+# NEAREST NEIGHBOR CLASSES
+###################################
+
+class NearestNeighbor:
+  class BACKEND(enum.Enum):
+    FALCONN = 1
+    FAISS = 2
+
+  def __init__(
+    self,
+    backend,
+    dimension,
+    neighbors,
+    number_bits,
+    nb_tables=None,
+  ):
+    assert backend in NearestNeighbor.BACKEND
+
+    self._NEIGHBORS = neighbors
+    self._BACKEND = backend
+
+    if self._BACKEND is NearestNeighbor.BACKEND.FALCONN:
+      self._init_falconn(
+        dimension,
+        number_bits,
+        nb_tables
+      )
+    elif self._BACKEND is NearestNeighbor.BACKEND.FAISS:
+      self._init_faiss(
+        dimension,
+      )
+    else:
+      raise NotImplementedError
+
+  def _init_falconn(
+    self,
+    dimension,
+    number_bits,
+    nb_tables,
+  ):
+    import falconn
+
+    assert nb_tables >= self._NEIGHBORS
+
+    # LSH parameters
+    params_cp = falconn.LSHConstructionParameters()
+    params_cp.dimension = dimension
+    params_cp.lsh_family = falconn.LSHFamily.CrossPolytope
+    params_cp.distance_function = falconn.DistanceFunction.EuclideanSquared
+    params_cp.l = nb_tables
+    params_cp.num_rotations = 2  # for dense set it to 1; for sparse data set it to 2
+    params_cp.seed = 5721840
+    # we want to use all the available threads to set up
+    params_cp.num_setup_threads = 0
+    params_cp.storage_hash_table = falconn.StorageHashTable.BitPackedFlatHashTable
+
+    # we build number_bits-bit hashes so that each table has
+    # 2^number_bits bins; a rule of thumb is to have the number
+    # of bins be the same order of magnitude as the number of data points
+    falconn.compute_number_of_hash_functions(number_bits, params_cp)
+    self._falconn_table = falconn.LSHIndex(params_cp)
+    self._falconn_query_object = None
+    self._FALCONN_NB_TABLES = nb_tables
+
+  def _init_faiss(
+    self,
+    dimension,
+  ):
+    import faiss
+
+    res = faiss.StandardGpuResources()
+
+    self._faiss_index = faiss.GpuIndexFlatL2(
+      res,
+      dimension,
+    )
+
+  def _find_knns_falconn(self, x, output):
+    # Late falconn query_object construction
+    # Since I suppose there might be an error
+    # if table.setup() will be called after
+    if self._falconn_query_object is None:
+      self._falconn_query_object = self._falconn_table.construct_query_object()
+      self._falconn_query_object.set_num_probes(
+        self._FALCONN_NB_TABLES
+      )
+
+    missing_indices = np.zeros(output.shape, dtype=np.bool)
+
+    for i in range(x.shape[0]):
+      query_res = self._falconn_query_object.find_k_nearest_neighbors(
+        x[i],
+        self._NEIGHBORS
+      )
+      try:
+        output[i, :] = query_res
+      except:  # pylint: disable-msg=W0702
+        # mark missing indices
+        missing_indices[i, len(query_res):] = True
+
+        output[i, :len(query_res)] = query_res
+
+    return missing_indices
+
+  def _find_knns_faiss(self, x, output):
+    neighbor_distance, neighbor_index = self._faiss_index.search(
+      x,
+      self._NEIGHBORS
+    )
+
+    missing_indices = neighbor_distance == -1
+
+    d1 = neighbor_index.reshape(-1)
+
+    output.reshape(-1)[
+      np.logical_not(missing_indices.flatten())
+    ] = d1[
+      np.logical_not(missing_indices.flatten())
+    ]
+
+    return missing_indices
+
+  def add(self, x):
+    if self._BACKEND is NearestNeighbor.BACKEND.FALCONN:
+      self._falconn_table.setup(x)
+    elif self._BACKEND is NearestNeighbor.BACKEND.FAISS:
+      self._faiss_index.add(x)
+    else:
+      raise NotImplementedError
+
+  def find_knns(self, x, output):
+    if self._BACKEND is NearestNeighbor.BACKEND.FALCONN:
+      return self._find_knns_falconn(x, output)
+    elif self._BACKEND is NearestNeighbor.BACKEND.FAISS:
+      return self._find_knns_faiss(x, output)
+    else:
+      raise NotImplementedError
 
 class NNGeod():
   def __init__(self, neighbors, proto_neighbors):
@@ -95,9 +233,13 @@ class NNGeod():
 
     return missing_indices
 
+###################################
+# GREAT MODEL
+###################################
+
 class DkNNModel(Model):
   def __init__(self, neighbors, proto_neighbors, layers, get_activations, train_data, train_labels,
-               nb_classes, method, scope=None, nb_tables=200):
+               nb_classes, method, backend=1, number_bits=17, scope=None, nb_tables=200):
     """
     Implements the DkNN algorithm. See https://arxiv.org/abs/1803.04765 for more details.
     :param neighbors: number of neighbors to find per layer.
@@ -114,6 +256,8 @@ class DkNNModel(Model):
     self.proto_neighbors = proto_neighbors
     self.method = method
     self.nb_tables = nb_tables
+    self.number_bits = number_bits
+    self.backend = backend
     self.layers = layers
     self.get_activations = get_activations
     self.nb_cali = -1
@@ -150,14 +294,12 @@ class DkNNModel(Model):
 
       if self.method == 'euclidean':
         print('Constructing the NearestNeighbor table')
-        # self.query_objects[layer] = NearestNeighbor(
-        #  backend=2,
-        #  dimension=self.train_activations_querier[layer].shape[1],
-        #  number_bits=self.number_bits,
-        #  neighbors=self.neighbors,
-        #  nb_tables=self.nb_tables
-        # )
-        # self.query_objects[layer].add(self.train_activations_querier[layer])
+        self.query_objects[layer] = NearestNeighbor(backend=self.backend,
+                                                    dimension=self.train_activations_querier[layer].shape[1],
+                                                    number_bits=self.number_bits,
+                                                    neighbors=self.neighbors,
+                                                    nb_tables=self.nb_tables)
+        self.query_objects[layer].add(self.train_activations_querier[layer])
 
       elif self.method == 'geodesic':
         print('Constructing the NearestNeighborGeodesic table')
